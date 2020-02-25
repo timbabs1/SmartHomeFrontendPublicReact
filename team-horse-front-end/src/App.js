efe3ae41ed22f214748a500dfc97e58dd5c78e56
Babz_Uni <haastrub@uni.coventry.ac.uk>
2020-02-25 18:54:30 +0000

    Auto stash before merge of "reactinitialisation" and "origin/reactinitialisation"

--- team-horse-front-end/src/App.js
+++ team-horse-front-end/src/App.js
@@ -6,21 +6,30 @@ import Header from './components/header/header';
 import Nav from './components/menu/menu';
 import LightSwitch from './components/lightswitch/switch';
 
-function App() {
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render(){
   return (
     <div className="App">
       <Header title="Smart-Hub 4 U" />
       <Nav />
       <div style={{ background: '#ECECEC', padding: '30px' }}>
         <LightSwitch /> <br/>
-        <Card title="Card title" bordered={false} style={{ width: 320 }}>
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
+        <Card title="Light Configuration" bordered={false} style={{ width: 320 }}>
+        <p>Brightness Level</p>
+        <p>Dim Level</p>
+        <p>Wattage</p>
         </Card>
         </div> 
     </div>
   );
+  }
 }
 
 export default App;
--- team-horse-front-end/src/components/lightswitch/switch.js
+++ team-horse-front-end/src/components/lightswitch/switch.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Switch, Button } from 'antd';
-import Websocket from '../websocket';
+import Websocket from 'react-websocket';
 
 class LightSwitch extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-          disabled: true
+          disabled: true,
+          text: 'Enable Edit Mode',
+          count: 0,
+          switch_digit: 1,
         }
     }
 
@@ -30,32 +33,65 @@ class LightSwitch extends React.Component {
       this.setState({
         disabled: !this.state.disabled,
       });
+      if (this.state.disabled === false){
+        this.setState({text: "Enable Edit Mode"}) 
+    } else {
+      this.setState({text: "Disable Edit Mode"})
+    }
     };
 
+    onChange = () => {
+          if (this.state.switch_digit === 1){
+            this.setState({switch_digit: 0}) 
+        } else {
+          this.setState({switch_digit: 1})
+        }
+        const message = {
+          Light_status : 0,
+          Room : "bedroom 1"
+        }
+      this.sendMessage(JSON.stringify(message))
+    }
+
+    handleData(data) {
+      let result = JSON.parse(data);
+      this.setState({count: result.movement});
+    }
+
+    handleOpen()  {
+      alert("connected:)");
+    }
+
+    handleClose() {
+      alert("disconnected:(");
+    }
+
+    sendMessage(message){
+      console.log(message.Room)
+      this.refWebSocket.sendMessage(message);
+      
+     /* let websocket = Websocket
+    websocket.sendMessage(message); */
+    }
 
   render() {
     return (
       <div>
         <h1>Switch toggle for auto lights</h1>
-        <Switch disabled={this.state.disabled} onClick={() => {
-          if(this.state.disabled === true){
-            this.sendMessage("off")
-          }else{
-            this.sendMessage("on")
-          }
-      }} defaultChecked /> 
+        Count: <strong>{this.state.count}</strong>
+        <Switch  onChange={this.onChange} checkedChildren="1" unCheckedChildren="0" disabled={this.state.disabled} /> <br/>
         <br/>
         <br/>
         <Button type="primary" onClick={this.toggle}>
-          Toggle disabled
+        {this.state.text}
         </Button>
+        <Websocket url='ws://localhost:8000/requestlight' onMessage={this.handleData.bind(this)}
+        onOpen={this.handleOpen} onClose={this.handleClose}
+        reconnect={true} debug={true}
+        ref={Websocket => {
+          this.refWebSocket = Websocket;
+        }}/>
 
-        <Websocket url='ws://localhost:8000/requestlight' onMessage={this.handleData}
-                onOpen={this.handleOpen} onClose={this.handleClose}
-                reconnect={true} debug={true}
-                ref={Websocket => {
-                  this.refWebSocket = Websocket;
-                }}/>
     </div>
         
     )

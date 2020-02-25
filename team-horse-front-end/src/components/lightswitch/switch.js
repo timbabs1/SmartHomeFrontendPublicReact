0da51f48046616c07d240a2563264fbdabcddbe1
GarethHolloway <hollow21@coventry.ac.uk>
2020-02-25 10:43:39 +0000

    Light switch and websocket functionalities.

--- team-horse-front-end/src/components/lightswitch/switch.js
+++ team-horse-front-end/src/components/lightswitch/switch.js
@@ -1,30 +1,61 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Switch, Button } from 'antd';
+import Websocket from '../websocket';
 
 class LightSwitch extends React.Component {
     constructor(props){
         super(props);
 
         this.state = {
-          disabled: true,
+          disabled: true
         }
     }
-    toggle = () => {
+
+    handleData(data) {
+      alert(data);
+    }
+    handleOpen()  {
+      alert("connected:)");
+    }
+    handleClose() {
+      alert("disconnected:(");
+    }
+
+    sendMessage(message){
+      this.refWebSocket.sendMessage(message);
+    }
+
+    toggle = (message) => {
       this.setState({
         disabled: !this.state.disabled,
       });
     };
 
+
   render() {
     return (
       <div>
         <h1>Switch toggle for auto lights</h1>
-        <Switch disabled={this.state.disabled} defaultChecked /> <br/>
+        <Switch disabled={this.state.disabled} onClick={() => {
+          if(this.state.disabled === true){
+            this.sendMessage("off")
+          }else{
+            this.sendMessage("on")
+          }
+      }} defaultChecked /> 
+        <br/>
         <br/>
         <Button type="primary" onClick={this.toggle}>
           Toggle disabled
         </Button>
+
+        <Websocket url='ws://localhost:8000/requestlight' onMessage={this.handleData}
+                onOpen={this.handleOpen} onClose={this.handleClose}
+                reconnect={true} debug={true}
+                ref={Websocket => {
+                  this.refWebSocket = Websocket;
+                }}/>
     </div>
         
     )
